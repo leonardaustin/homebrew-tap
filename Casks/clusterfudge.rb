ded68be96e0dc8f18018219fe829ffc9e6d9df0d
Leonard Austin <leonardaustin@users.noreply.github.com>
2026-03-23 22:35:32 +0000

    clusterfudge 2026.0323.2219

--- Casks/clusterfudge.rb
+++ Casks/clusterfudge.rb
@@ -2,16 +2,16 @@
 # frozen_string_literal: true
 
 cask "clusterfudge" do
-  version "2026.0322.2300"
+  version "2026.0323.2219"
 
   on_arm do
-    sha256 "0cd07dfc61c582d08fd56b6155e4c7933f8f4bbf4d3e8af2fb1442a6007afb91"
-    url "https://github.com/leonardaustin/clusterfudge/releases/download/v2026.0322.2300/clusterfudge_darwin_arm64.dmg"
+    sha256 "b00df67d6063b079c4c8916d7795a4e5f6e58b70a85b081ed8743b71ade76446"
+    url "https://github.com/leonardaustin/clusterfudge/releases/download/v2026.0323.2219/clusterfudge_darwin_arm64.dmg"
   end
 
   on_intel do
-    sha256 "c384cf9a9dbcc9e805c79bb02b1bfb8f945451bf9db87c5ab7ebaad5875e4768"
-    url "https://github.com/leonardaustin/clusterfudge/releases/download/v2026.0322.2300/clusterfudge_darwin_amd64.dmg"
+    sha256 "ab24d08b7e1884daa296c73da0b5182481c2791408967155129cbf0cc60705a6"
+    url "https://github.com/leonardaustin/clusterfudge/releases/download/v2026.0323.2219/clusterfudge_darwin_amd64.dmg"
   end
 
   name "Clusterfudge"
