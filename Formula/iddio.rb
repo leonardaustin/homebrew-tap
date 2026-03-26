33c563344a494323dab6b99663d085f1d9d5d19d
Leonard Austin <leonardaustin@users.noreply.github.com>
2026-03-26 15:53:23 +0000

    iddio 2026.0326.1537

--- Formula/iddio.rb
+++ Formula/iddio.rb
@@ -4,27 +4,27 @@
 class Iddio < Formula
   desc "Security gateway for AI agent access to Kubernetes clusters"
   homepage "https://iddio.dev"
-  version "2026.0224.0009"
+  version "2026.0326.1537"
 
   on_macos do
     on_intel do
-      url "https://github.com/leonardaustin/iddio/releases/download/v2026.0224.0009/iddio_2026.0224.0009_darwin_amd64.tar.gz"
-      sha256 "88bbac10384a035caaa1cbf0dfc3c4016b49261ee7552a93270aa4b1b2247cb7"
+      url "https://github.com/leonardaustin/iddio/releases/download/v2026.0326.1537/iddio_2026.0326.1537_darwin_amd64.tar.gz"
+      sha256 "6e57e378340c0a7331266f99474cbd89cd0c78f805c48ba98ee30f67d9583989"
     end
     on_arm do
-      url "https://github.com/leonardaustin/iddio/releases/download/v2026.0224.0009/iddio_2026.0224.0009_darwin_arm64.tar.gz"
-      sha256 "e7d90ed701f53766f01588e7fa4c099ec46f74cac76e746d528c6a0c6750f5e6"
+      url "https://github.com/leonardaustin/iddio/releases/download/v2026.0326.1537/iddio_2026.0326.1537_darwin_arm64.tar.gz"
+      sha256 "423f34b53add9f8323918efca3e0d2ad35b3763c05e6b2e2aff407b7c870d437"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/leonardaustin/iddio/releases/download/v2026.0224.0009/iddio_2026.0224.0009_linux_amd64.tar.gz"
-      sha256 "aa99e3eb5dabef14aa699dad61be1a833335c1cfab88a8290ef7db6ba0c33599"
+      url "https://github.com/leonardaustin/iddio/releases/download/v2026.0326.1537/iddio_2026.0326.1537_linux_amd64.tar.gz"
+      sha256 "4e2b8cd27e30a790ce242d1baebbf5dc8381dec2985be68b8a0ea0dee251a718"
     end
     on_arm do
-      url "https://github.com/leonardaustin/iddio/releases/download/v2026.0224.0009/iddio_2026.0224.0009_linux_arm64.tar.gz"
-      sha256 "11b0a5b76964dba611e57e88e6deb57175f7adb12f197949c3cbb439d7a510cc"
+      url "https://github.com/leonardaustin/iddio/releases/download/v2026.0326.1537/iddio_2026.0326.1537_linux_arm64.tar.gz"
+      sha256 "0b20baccad34c3b21f2c6b612554d13e38894baabd7cce6e22e6c0ef7c7a282d"
     end
   end
 
