b103a87f1e1387505470cebb7ca6307dbda1229f
Leonard Austin <leonardaustin@users.noreply.github.com>
2026-05-26 21:31:39 +0000

    iddio-desktop 2026.05.26.2115

--- Casks/iddio-desktop.rb
+++ Casks/iddio-desktop.rb
@@ -2,16 +2,16 @@
 # frozen_string_literal: true
 
 cask "iddio-desktop" do
-  version "2026.526.1759"
+  version "2026.05.26.2115"
 
   on_arm do
-    sha256 "0c7997da4f957a2210a453e113b0cb64f458f5e030ec05f55f3811a354034779"
-    url "https://github.com/leonardaustin/iddio/releases/download/v26.0526.1759/iddio-desktop_v26.0526.1759_darwin_arm64.dmg"
+    sha256 "34917908fc63b73e0881ab6d4c087588fa136ed442c3b94a361f40808a146f36"
+    url "https://github.com/leonardaustin/iddio/releases/download/v26.0526.2115/iddio-desktop_v26.0526.2115_darwin_arm64.dmg"
   end
 
   on_intel do
-    sha256 "6d3eb96744f59895648faf4c211ae7cd2c8febc299662fce91d6fad5c8cb5bf5"
-    url "https://github.com/leonardaustin/iddio/releases/download/v26.0526.1759/iddio-desktop_v26.0526.1759_darwin_amd64.dmg"
+    sha256 "cbfde853904868db388ec050173a5ca8d1f37c90fac64465b79f92ebe6f5a547"
+    url "https://github.com/leonardaustin/iddio/releases/download/v26.0526.2115/iddio-desktop_v26.0526.2115_darwin_amd64.dmg"
   end
 
   name "Iddio"
